8fb0ccd933b6dd14194da2a08e09b1f6ddeb8683
Beibarys Learner <beibarys.learner@gmail.com>
2023-07-12 11:26:25 +0600

    day1 and day2 problems testing

---
+++ src/kz/java/problems/day1/Main.java
@@ -0,0 +1,7 @@
+package kz.java.problems.day1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/kz/java/problems/day2/Main.java
@@ -0,0 +1,7 @@
+package kz.java.problems.day2;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Terminator");
+    }
+}
